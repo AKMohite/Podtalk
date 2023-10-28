302dddf3b98065b36039458497128046b3faf7fc
Ashish Mohite <ashishmohite1996@gmail.com>
2023-10-28 20:49:43 +0530

    setup tab bar items

--- Podtalks/Core/ViewController/PodtalkTabViewController.swift
+++ Podtalks/Core/ViewController/PodtalkTabViewController.swift
@@ -20,13 +20,29 @@ final class PodtalkTabViewController: UITabBarController {
         let libraryVC = LibraryViewController()
         let settingsVC = SettingsViewController()
         
+        discoverVC.navigationItem.largeTitleDisplayMode = .automatic
+        searchVC.navigationItem.largeTitleDisplayMode = .automatic
+        libraryVC.navigationItem.largeTitleDisplayMode = .automatic
+        settingsVC.navigationItem.largeTitleDisplayMode = .automatic
+        
         let discoverNav = UINavigationController(rootViewController: discoverVC)
         let searchNav = UINavigationController(rootViewController: searchVC)
         let libraryNav = UINavigationController(rootViewController: libraryVC)
         let settingsNav = UINavigationController(rootViewController: settingsVC)
         
+        discoverNav.tabBarItem = UITabBarItem(title: "Discover", image: UIImage(systemName: "rectangle.3.offgrid"), tag: 1)
+        searchNav.tabBarItem = UITabBarItem(title: "Search", image: UIImage(systemName: "magnifyingglass"), tag: 2)
+        libraryNav.tabBarItem = UITabBarItem(title: "Library", image: UIImage(systemName: "folder"), tag: 3) // music.note.list
+        settingsNav.tabBarItem = UITabBarItem(title: "Settings", image: UIImage(systemName: "gear"), tag: 4)
+        
+        let rootControllers = [discoverNav, searchNav, libraryNav, settingsNav]
+        
+        for controller in rootControllers {
+            controller.navigationBar.prefersLargeTitles = true
+        }
+        
         setViewControllers(
-            [discoverNav, searchNav, libraryNav, settingsNav],
+            rootControllers,
             animated: true
         )
     }
