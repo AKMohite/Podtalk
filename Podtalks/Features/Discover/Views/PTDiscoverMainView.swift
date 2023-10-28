2effd22ede4a17434739e5beae66e18974803a0a
Ashish Mohite <ashishmohite1996@gmail.com>
2023-10-28 22:29:54 +0530

    Add main view

---
+++ Podtalks/Features/Discover/Views/PTDiscoverMainView.swift
@@ -0,0 +1,21 @@
+//
+//  PTDiscoverMainView.swift
+//  Podtalks
+//
+//  Created by Ashish Mohite + on 28/10/23.
+//
+
+import UIKit
+
+class PTDiscoverMainView: UIView {
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("Not loaded by: PTDiscoverMainView")
+    }
+    
+    
+}
