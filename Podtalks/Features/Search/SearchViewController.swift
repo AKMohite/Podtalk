c0c4803ea75073da291ff0b41d8394685f5a6095
Ashish Mohite <ashishmohite1996@gmail.com>
2023-11-08 21:28:05 +0530

    Add search view

--- Podtalks/Features/Search/SearchViewController.swift
+++ Podtalks/Features/Search/SearchViewController.swift
@@ -8,10 +8,23 @@
 import UIKit
 
 final class SearchViewController: UIViewController {
+    
+    private let searchView = PTSearchMainView()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .systemBlue
+        view.backgroundColor = .systemBackground
         title = "Search"
+        view.addSubview(searchView)
+        addConstraints()
+    }
+    
+    private func addConstraints() {
+        NSLayoutConstraint.activate([
+            searchView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+            searchView.leftAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leftAnchor),
+            searchView.rightAnchor.constraint(equalTo: view.safeAreaLayoutGuide.rightAnchor),
+            searchView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor)
+        ])
     }
 }
